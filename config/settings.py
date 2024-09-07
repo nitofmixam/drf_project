1fc9f7c5e9efa4201c705cd99704c0ad2036b489
Maksim Fotin <maksimfotin144@gmail.com>
2024-09-07 21:03:12 +0700

    Homework_25.2

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,7 @@ AUTH_USER_MODEL = "users.User"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
